a44a53babf18da902fa7925ed5a6de80dc158bc4
Camille <camille.demers@umontreal.ca>
2023-08-14 14:55:05 -0400

    complété 'expertises' (ce quil y avait deja)

--- generate_html.py
+++ generate_html.py
@@ -1,14 +1,14 @@
-from dash import Dash, html, dcc, dash_table
-import plotly.express as px
-import plotly.io as pio
-import plotly.graph_objects as go
-import pandas as pd
-import dash_bootstrap_components as dbc
-from utils.sadvr_utils import *
-from generate_figures import *
+# from dash import Dash, html, dcc, dash_table
+# import plotly.express as px
+# import plotly.io as pio
+# import plotly.graph_objects as go
+# import pandas as pd
+# import dash_bootstrap_components as dbc
+# from utils.sadvr_utils import *
+# from generate_figures import *
 
 # utils to generate html file
-tableClasses = ['table', 'table-bordered', 'table-hover', 'table-sm']
+tableClasses = ['table', 'table-hover']
 
 with open('./html/header.html', encoding='utf-8') as f:
     header = f.read()
@@ -21,7 +21,7 @@ with open('./html/footer.html') as f:
 
 
 ### Générer le fichier html pour les affiliations
-with open('html/affiliations.html', 'w', encoding='utf-8') as f:
+with open('index.html', 'w', encoding='utf-8') as f:
     f.write(header)
     f.write('<h2>Affiliations</h2></div>')
 
@@ -73,48 +73,55 @@ with open('html/affiliations.html', 'w', encoding='utf-8') as f:
         <div class="grid">
             <div class="col-md-4" 
                 style="float:left; padding-right:5px; 
-                margin-top:10px; max-height:425px; overflow-y:scroll;">
+                margin-top:10px; height:430px; max-height:430px; overflow-y:scroll;">
         """
     )
-    f.write(departements.to_html(
+    f.write(departements[['Département', 'N']].to_html(
         index=False, classes=tableClasses, justify='left')
     )
     
-    f.write('</div><div class="col-md-8" style="float:right; padding-right:20px;">')
+    f.write('</div><div class="col-md-8" style="float:right; padding-right:20px;padding-left:20px; max-height:430px; overflow-y:scroll;">')
 
     f.write(figDepartements.to_html(full_html=False, include_plotlyjs='cdn'))
     f.write('</div></div>')
-    f.write('<div style="margin-top:500px;"><hr/>')
+    f.write('<div style="margin-top:600px;"><hr/>')
 
     f.write(footer)
 
 ### Générer le fichier html pour les unités de recherche
-with open('html/expertises.html', 'w', encoding='utf-8') as f:
+with open('expertises.html', 'w', encoding='utf-8') as f:
     f.write(header)
-    f.write('<h2>Affiliations</h2></div>')
-
-    # Établissements affiliés
-    f.write('<h3>Établissements affiliés</h3>')
-    f.write(
-        """
-        <div class="grid">
-            <div class="col-md-4" 
-                style="float:left; padding-right:5px; 
-                margin-top:10px; max-height:425px; overflow-y:scroll;">
-        """
-    )
+    f.write('<h2>Expertises de recherche</h2></div>')
 
     # Disciplines de recherche
     f.write('<h3>Disciplines</h3>')
-    f.write("<p>Principales disciplines de recherche à l'UdeM (Top 30)</p>")
-    f.write(figDisciplines.to_html(full_html=False, include_plotlyjs='cdn'))
-    f.write('<div/">')
-    f.write('<div style="margin-top:80px; <hr/>">')
+    f.write(
+        """
+        <div class="grid">
+            <div class="col-md-5" 
+                style="float:left; padding-right:5px; 
+                margin-top:10px; max-height:450px; overflow-y:scroll;">
+        """
+    )
+
+    f.write("<p>Nombre de professeur-e-s par discipline</p>")
+    table = tableDisciplines[['Discipline', 'N']].to_html(
+        index=False, classes=tableClasses, justify='left'
+    )
+    table = BeautifulSoup(table, 'html.parser')
+    table.find('table')['style'] = 'max-heigth:400px; height:300px; overflow-y: scroll;'
+    f.write(str(table)
+    )
+    f.write('</div><div class="col-md-6" style="float:right; margin-right:100px;">')
     f.write(figDisciplinesFacultes.to_html(full_html=False, include_plotlyjs='cdn'))
-    f.write('<div/">')
+
+    f.write('</div><div class="col-md-8" style="float:right; padding-right:20px;">')
+    f.write('</div></div>')
+    f.write('<div style="margin-top:500px;"><hr/></div>')
+
+     # Disciplines de recherche
+    f.write('<h3>Mots-clés</h3>')
     f.write(footer)
 
 
 ### Générer le fichier html pour les expertises de recherche
-
-
